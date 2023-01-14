8c80f208c99ad2dbf0377c910cfc2bdd1cfd7c1b
itbeginshere <59728343+itbeginshere@users.noreply.github.com>
2023-01-14 15:39:43 +0200

    some testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,57 @@
-import './App.css';
+import { useState, useCallback, useMemo } from 'react';
+import ReactFlow, { Controls, Background, Node, Edge, applyEdgeChanges, applyNodeChanges, NodeChange, EdgeChange, addEdge, Connection } from 'reactflow';
+import 'reactflow/dist/style.css';
+import TextUpdaterNode from './components/TextUpdaterNode';
+
+const initialNodes : Array<Node> = [
+  {
+    id: '1', 
+    position: { x: 0, y: 0 },
+    data: { label: 'Hello' },
+    type: 'input',
+  }, 
+  {
+    id: '2',
+    position: { x: 100, y: 100 },
+    type: 'textUpdater',
+    data: { value: 'World' },
+  }
+]
+
+const initialEdges : Array<Edge> = [];
 
 function App() {
+
+  const [nodes, setNodes] = useState<Array<Node>>(initialNodes);
+  const [edges, setEdges] = useState<Array<Edge>>(initialEdges);
+
+  const onNodesChange = useCallback((changes: Array<NodeChange>) => 
+    setNodes((prev) => applyNodeChanges(changes, prev)
+  ), []);
+
+  const onEdgesChange = useCallback((changes : Array<EdgeChange>) => 
+    setEdges((prev) => applyEdgeChanges(changes, prev)
+  ), [])
+  
+  const onConnect = useCallback((params: Edge | Connection) => 
+    setEdges((eds) => addEdge(params, eds)
+  ), []);
+
+  const nodeTypes = useMemo(() => ({ 'textUpdater' : TextUpdaterNode}), []);
+
   return (
-    <div>
+    <div className={'h-full'}>
+      <ReactFlow 
+        nodes={nodes} 
+        edges={edges}
+        nodeTypes={nodeTypes}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
     </div>
   )
 }
